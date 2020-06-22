7f844310770a9c8246e74ecd8083a9ab2e5b20e1
nskourlis <48327071+nskourlis@users.noreply.github.com>
2020-06-22 18:14:46 +0200

    Initial try
    Initial try

---
+++ R_boxplot_final.R
@@ -0,0 +1,252 @@
+
+#install.packages("tidyverse")
+#install.packages("tidyr")
+#install.packages("dplyr")
+#install.packages("magrittr")
+
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+library(gridExtra)
+library(grid)
+library(tidyverse)
+library(ggplot2)
+library(shiny)
+library(plotly)
+library(DT)
+library(magrittr)
+
+
+setwd("C:/Users/niksko/Desktop/Simulation_mod/Simulation_new2_notvc_a/8.Bias_tables/Boxplots_R/")
+##########################################################################################
+#Boxplot
+#############################################################################################
+data <- read.csv("R_long.csv", header = TRUE, sep = ",", dec = ".")
+data[1:10,]
+attach(data)
+
+data$t5  <- ifelse(year ==5, TRUE, FALSE)
+data$t10 <- ifelse(year ==10, TRUE, FALSE)
+
+data$bias_CIF2_at[which(data$scenario==14 & data$year==5 & data$model==0 & data$givenage==70 )]
+data$bias_CIF2_at[which(data$scenario==14 & data$year==5 & data$model==0 & data$givenage==80)]
+
+
+
+###############CIF1##########################################################################################
+options(scipen = 999)
+
+
+data$id_n =rep(1:(nrow(data)/10), times=1, each=10)
+
+for (i in 1:(nrow(data)/10)) {data$t5_CIF1[(((i*10)-10)+1):((i*10))]= data[which(data$year==5 & data$t5==TRUE & data$id_n==i),23] }
+for (i in 1:(nrow(data)/10)) {data$t10_CIF1[(((i*10)-10)+1):((i*10))]= data[which(data$year==10 & data$t10==TRUE & data$id_n==i),23] }
+
+for (i in 1:(nrow(data)/10)) {data$t5_CIF2[(((i*10)-10)+1):((i*10))]= data[which(data$year==5 & data$t5==TRUE & data$id_n==i),24] }
+for (i in 1:(nrow(data)/10)) {data$t10_CIF2[(((i*10)-10)+1):((i*10))]= data[which(data$year==10 & data$t10==TRUE & data$id_n==i),24] }
+
+
+data$code = as.factor(c(rep("Age at diagnosis 70", nrow(data))))
+data$model=as.factor(data$model)
+
+data70=data.frame(data[givenage==70 & scenario<=12,])
+data70$bias_CIF2_at[which(data70$scenario==1 & data70$year==5 & data70$model==0 )]
+
+data80=data[givenage==80 & scenario<=12,]
+data80$bias_CIF2_at[which(data80$scenario==1 & data80$year==5 & data80$model==0 )]
+
+
+
+
+
+p1=ggplot(aes(y = bias_CIF1_at, x = factor(scenario), fill = as.factor(model)), data = data70) +
+  geom_boxplot() +
+  scale_fill_discrete(name="Approaches",
+                      breaks=c(0,1,2,3),
+                      labels=c("Attained", "Linear", "Splines","Splines/int"))+
+  xlab("Scenarios")+ylab("Bias CIF1") + 
+  #geom_rect(aes(xmin = 0.5,xmax =4.5,ymin = 0.001, ymax = 0.008, fill = "#d8161688"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 7.5,xmax =12.5,ymin = 0.001, ymax = 0.008, fill = "#d8161688"),size = 2 , alpha=0.006)+
+  # geom_rect(aes(xmin = 13.5,xmax =14.5,ymin = 0, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.001)+
+  # geom_rect(aes(xmin = 22.5,xmax =25,ymin = -0.002, ymax = 0.005, fill = "#springgreen2"),size = 2 , alpha=0.005)+
+  labs(title = "Age at diagnosis 70",
+       x = "Scenarios",
+       color = "") +
+  geom_point(data = data70,position=position_dodge(width=0.75), mapping=aes(x =factor(scenario), y = mean_bias_CIF1_at,  fill = model, pch = 16, color = "black"),  
+             size = 1) +
+  geom_point(data = data70,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y = t5_CIF1, fill = model,  pch = 16, color = "orange4"),  
+             size = 1) +
+  geom_point(data = data70,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y =t10_CIF1, fill = model, pch = 16, color = "red"),  
+             size = 1) +
+  scale_color_manual(values = c("black", "orange4", "red"),
+                     labels = c("Mean bias CIF1", "Bias CIF1 at time since diag= 5","Bias CIF1 at time since diag=10"  )) +
+  scale_shape_identity()+
+  theme(panel.grid.major = element_blank(),plot.margin = margin(8,0,8,0),
+        axis.title.x = element_text( size=18),
+        axis.title.y = element_text( size=18),
+        axis.text.x = element_text( 
+          size=13),
+        axis.text.y = element_text( 
+          size=13))+ 
+  scale_y_continuous(breaks =seq( -0.005, 0.005, by = 0.001)) +
+  theme(legend.position='none',legend.text = element_text( size=10))+theme(axis.title.x=element_blank())
+
+
+p1
+
+
+
+p2=ggplot(aes(y = bias_CIF1_at, x = factor(scenario), fill = factor(model)), data = data80) +
+  geom_boxplot() +
+  scale_fill_discrete(name="Approaches",
+                                       breaks=c(0,1,2,3),
+                                       labels=c("Attained", "Linear", "Splines","Splines/int"))+
+  xlab("Scenarios")+ylab("Bias CIF1") + 
+  #geom_rect(aes(xmin = 0.5,xmax =4.5,ymin = 0.001, ymax = 0.008, fill = "#d8161688"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 7.5,xmax =12.5,ymin = 0.001, ymax = 0.008, fill = "#d8161688"),size = 2 , alpha=0.006)+
+ # geom_rect(aes(xmin = 13.5,xmax =14.5,ymin = 0, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.001)+
+ # geom_rect(aes(xmin = 22.5,xmax =25,ymin = -0.002, ymax = 0.005, fill = "#springgreen2"),size = 2 , alpha=0.005)+
+  labs(title = "Age at diagnosis 80",
+       x = "Scenarios",
+       color = "") +
+  geom_point(data = data80,position=position_dodge(width=0.75), mapping=aes(x =factor(scenario), y = mean_bias_CIF1_at,  fill = model, pch = 16, color = "black"),  
+             size = 1) +
+  geom_point(data = data80,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y = t5_CIF1, fill = model,  pch = 16, color = "orange4"),  
+             size = 1) +
+  geom_point(data = data80,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y =t10_CIF1, fill = model, pch = 16, color = "red"),  
+             size = 1) +
+  scale_color_manual(values = c("black", "orange4", "red"),
+                     labels = c("Mean bias CIF1", "Bias CIF1 at time since diag= 5","Bias CIF1 at time since diag=10"  )) +
+  scale_shape_identity()+
+  theme(panel.grid.major = element_blank(),plot.margin = margin(8,0,8,0),
+        axis.title.x = element_text( size=18),
+        axis.title.y = element_text( size=18),
+        axis.text.x = element_text( 
+          size=13),
+        axis.text.y = element_text( 
+          size=13))+ 
+  scale_y_continuous(breaks =seq( -0.008, 0.01, by = 0.002)) +
+theme(legend.position="bottom",legend.text = element_text( size=10) ) 
+
+p2
+
+
+
+png("boxplot_CIF1_overall.png",height=1000, width=1000)
+
+grid.arrange(p1 ,p2 , nrow=2, heights=c(1,1), bottom = 
+               textGrob("",
+                        x = 0,y = 0.5, just = "left"))
+
+dev.off()
+#Figure 1: Boxplots of bias over the 10 years of follow-up time in CIF1 from each approach over the scenarios",x = 0,y = 0.5, just = "left",gp=gpar(fontsize=16,font=1))
+
+p1_ly <- ggplotly(p1)
+#p1_ly
+
+p2_ly <- ggplotly(p2)
+#p2_ly
+
+
+###############CIF2##########################################################################################
+
+p3=ggplot(aes(y = bias_CIF2_at, x = factor(scenario), fill = as.factor(model)), data = data70) +
+  geom_boxplot()+  scale_fill_discrete(name="Approaches",
+                                       breaks=c(0,1,2,3),
+                                       labels=c("Attained", "Linear", "Splines","Splines/int"))+
+  ylab("Bias CIF2") + 
+  #geom_rect(  aes(xmin = 12.5,xmax =13.5,ymin = 0, ymax = 0.02, fill = "#d8161688"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 14.5,xmax =15.5,ymin = 0, ymax = 0.02, fill = "#d8161688"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 20.5,xmax =21.5,ymin = 0, ymax = 0.02, fill = "#d8161688"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 22.5,xmax =23.5,ymin = 0, ymax = 0.02, fill ="#d8161688"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 13.5,xmax =14.5,ymin = 0, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 15.5,xmax =16.5,ymin = 0, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 21.5,xmax =22.5,ymin = 0, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 23.5,xmax =24.5,ymin = 0, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 5.5 ,xmax =6.5, ymin = 0, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 7.5 ,xmax =8.5, ymin = 0, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 17.5,xmax =18.5,ymin = 0, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 19.5,xmax =20.5,ymin = 0, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  labs(title = "Age at diagnosis 70",
+       color = "") +
+  geom_point(data = data70,position=position_dodge(width=0.75), mapping=aes(x =factor(scenario), y = mean_bias_CIF2_at,  fill = model, pch = 16, color = "black"),  
+             size = 1) +
+  geom_point(data = data70,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y = t5_CIF2, fill = model,  pch = 16, color = "orange4"),  
+             size = 1) +
+  geom_point(data = data70,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y =t10_CIF2, fill = model, pch = 16, color = "red"),  
+             size = 1) +
+  scale_color_manual(values = c("black", "orange4", "red"),
+                     labels = c("Mean bias CIF2","Bias CIF2 at time since diagnosis= 5","Bias CIF2 at time since diagnosis=10"  )) +
+  scale_shape_identity()+
+  theme(panel.grid.major = element_blank(),plot.margin = margin(8,0,8,0),
+        axis.title.x = element_text( size=18),
+        axis.title.y = element_text( size=18),
+        axis.text.x = element_text( 
+          size=13),
+        axis.text.y = element_text( 
+          size=13))+ 
+  scale_y_continuous(breaks =seq( -0.01, 0.05, by = 0.01)) +
+  theme(legend.position='none')+theme(axis.title.x=element_blank())
+  
+p3
+
+p4=ggplot(aes(y = bias_CIF2_at, x = factor(scenario), fill = as.factor(model)), data = data80) +
+  geom_boxplot()+  scale_fill_discrete(name="Approaches",
+                                       breaks=c(0,1,2,3),
+                                       labels=c("Attained", "Linear", "Splines","Splines/Int"))+
+  xlab("Scenarios")+ylab("Bias CIF2") +
+  #geom_rect(aes(xmin = 1.5,xmax =2.5,ymin = -0.011, ymax = 0.015, fill = "#d8161688"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 3.5,xmax =4.5,ymin = -0.011, ymax = 0.015, fill = "#d8161688"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 9.5,xmax =10.5,ymin = -0.011, ymax = 0.015, fill = "#d8161688"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 11.5,xmax =12.5,ymin = -0.011, ymax = 0.015, fill ="#d8161688"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 13.5,xmax =14.5,ymin = -0.01, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 15.5,xmax =16.5,ymin = -0.01, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 21.5,xmax =22.5,ymin = -0.01, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 23.5,xmax =24.5,ymin = -0.01, ymax = 0.02, fill = "#deepskyblue1"),size = 2 , alpha=0.001)+
+  #geom_rect(aes(xmin = 5.5, xmax =6.5, ymin = -0.015, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 7.5, xmax =8.5, ymin = -0.015, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 13.5,xmax =14.5,ymin = -0.015, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 15.5,xmax =16.5,ymin = -0.015, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 21.5,xmax =22.5,ymin = -0.015, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  #geom_rect(aes(xmin = 23.5,xmax =24.5,ymin = -0.015, ymax = 0.02, fill = "#springgreen2"),size = 2 , alpha=0.006)+
+  labs(title = "Age at diagnosis 80",
+       x = "Scenarios",
+       color = "") +
+  geom_point(data = data80,position=position_dodge(width=0.75), mapping=aes(x =factor(scenario), y = mean_bias_CIF2_at,  fill = model, pch = 16, color = "black"),  
+             size = 1) +
+  geom_point(data = data80,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y = t5_CIF2, fill = model,  pch = 16, color = "orange4"),  
+             size = 1) +
+  geom_point(data = data80,position=position_dodge(width=0.75), mapping=aes(x = factor(scenario), y =t10_CIF2, fill = model, pch = 16, color = "red"),  
+             size = 1) +
+  scale_color_manual(values = c("black", "orange4", "red"),
+                     labels = c("Mean bias CIF2", "Bias CIF2 at time since diag= 5","Bias CIF2 at time since diag=10"  )) +
+  scale_shape_identity()+
+  theme(panel.grid.major = element_blank(),plot.margin = margin(8,0,8,0),
+        axis.title.x = element_text( size=18),
+        axis.title.y = element_text( size=18),
+        axis.text.x = element_text( 
+          size=13),
+        axis.text.y = element_text( 
+          size=13))+ 
+  scale_y_continuous(breaks =seq( -0.03, 0.015, by = 0.01),limits=c(-0.03, 0.015)) +
+  theme(legend.position="bottom",legend.text = element_text( size=10) ) 
+  
+p4
+
+
+png("boxplot_CIF2_overall.png",height=1000, width=1000)
+
+grid.arrange(  p3,p4, nrow=2,  heights=c(1,1),
+               bottom = textGrob("",                                                             
+                                 x = 0,y = 0.5,just = "left")      )
+
+
+dev.off()
+#top=textGrob("Figure 2: Boxplots of bias over the 10 years of follow-up time in CIF2 from each approach over the scenarios",x = 0,y = 0.5, just = "left",gp=gpar(fontsize=16,font=1))
+
+#p1_ly <- ggplotly(p1)
+#p1_ly
+
+#p2_ly <- ggplotly(p2)
+#p2_ly
+
